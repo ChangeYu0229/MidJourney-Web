62e05849860cada768c3186e4fdba93ab5fb9e0d
Aiden <42216064+ChangeYu0229@users.noreply.github.com>
2023-08-03 23:50:58 -0400

    Update SendArea.tsx

--- src/components/SendArea.tsx
+++ src/components/SendArea.tsx
@@ -74,9 +74,9 @@ const SendArea = forwardRef(({
       <div className="fixed left-1/2 transform -translate-x-1/2 bottom-40px md:max-w-[90%] md:w-[560px] sm:sm:w-[90%] backdrop-blur-md pt-1 px-4 pb-4 z-100 text-[16px] rounded-md">
         {ifLoading ? (
           <div className="gen-cb-wrapper">
-            <span>Thinking...</span>
+            <span>正在出图中...</span>
             <div className="gen-cb-stop" onClick={handleStopClick}>
-              Stop
+              停止
             </div>
           </div>
         ) : (
