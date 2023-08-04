fcc00b134786fb67675f737c2690b39efbe11490
Aiden <42216064+ChangeYu0229@users.noreply.github.com>
2023-08-03 23:48:31 -0400

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,7 +15,6 @@ const Header = () => {
             </div>
             <p className="mt-1 opacity-60 ">
                 Powered by
-                <br /> {/* 或者使用 "\n" 换行符 */}
                 <a
                     className="b-buy-link ml-1 decoration-wavy underline-offset-8"
                     href={ GITHUB_ORG_URL } target="_blank"
@@ -23,7 +22,7 @@ const Header = () => {
                 >
                      AI 实验室
                 </a>
-            </p>
+            </p>    <br /> {/* 或者使用 "\n" 换行符 */}
             <p>免费体验版高峰期出图慢是正常现象，请勿重复提交</p>
         </header>
     );
