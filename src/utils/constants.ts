6511d9058cc9fa21678fbed02a8a21b987b45241
Aiden <42216064+ChangeYu0229@users.noreply.github.com>
2023-09-03 18:52:23 -0400

    Update constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,6 +3,6 @@ export const SITE_DESC = 'Supercharged Experience For MidJourney On Web UI'
 
 export const MAX_TOKENS = 500
 export const RATE_LIMIT_COUNT = 5
-export const OPS_URL = 'https://www.bing666.top/'
+export const OPS_URL = 'https://kl.aigc369.com'
 export const GITHUB_ORG_URL = 'https://link3.cc/aipro'
 
